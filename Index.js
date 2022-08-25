bdf5c4ed78ad0d1bb08376ac5494523f87d2b621
Joshua Rei <xxstubzxx@gmail.com>
2022-08-25 17:46:23 -0400

    Convert commonJs to esModules
    Along with converting to esModules this also puts a fix in for the non-windows system to define the window key within the globalThis.

--- Index.js
+++ Index.js
@@ -1,14 +1,16 @@
 `use strict`;
 
-const {Command} = require(`commander`);
+import { Command } from 'commander';
 
 const Program = new Command();
+
+import { MapCommands } from './Commands.js';
 
 Program
     .name(`Glove Box`)
     .description(`A CLI tool with multiple utilities for working with Salesforce data and systems.`)
     .version(`1.0.0`);
 
-require(`./Commands`).Map(Program);
+MapCommands(Program);
 
 Program.parse();
