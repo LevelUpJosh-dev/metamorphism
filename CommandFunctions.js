864f4cdcdd7dbef454de4b94d8d254a9a7a5edc5
Joshua Rei <xxstubzxx@gmail.com>
2022-08-30 23:19:55 -0400

    CSV Methods
    Includes jsonify_csv and csvify_json methods also has WIP for stringify_pdf method textify_html method as well.

--- CommandFunctions.js
+++ CommandFunctions.js
@@ -9,13 +9,17 @@ import * as json2yaml from 'json-to-pretty-yaml';
 import * as json_format from 'json-format';
 import showdown from 'showdown';
 import xml2js from 'xml2js';
-import { JSDOM } from 'jsdom';
+import { JSDOM } from 'jsdom'
+import Papaparse from 'papaparse'
+import { htmlToText } from 'html-to-text';
+import * as Pdf from 'pdf-parse';
 
 const tomlParser = json2toml.default;
 
 /**
  * Takes a path to an XML file which will be transformed into JSON
  * @param { Object } options - Program options
+ * @param { String } options.path - Path to XML file to be morphed JSON
  **/
 const jsonify_xml = (options) => {
     const xml_string = fs.readFileSync(options.path, { "encoding": "utf8" });
@@ -32,6 +36,7 @@ const jsonify_xml = (options) => {
 /**
  * Takes a path to a JSON file which will be transformed into XML
  * @param { Object } options - Program options
+ * @param { String } options.path - Path to JSON file to be morphed XML
  **/
 const xmlify_json = (options) => {
     const xml_json = JSON.parse(fs.readFileSync(options.path, { "encoding": "utf8" }));
@@ -43,6 +48,7 @@ const xmlify_json = (options) => {
 /**
  * Takes a path to a JSON file which will be transformed into YAML
  * @param { Object } options - Program options
+ * @param { String } options.path - Path to JSON file to be morphed into YAML
  **/
 const yamlify_json = (options) => {
     const json_string = fs.readFileSync(options.path, { "encoding": "utf8" });
@@ -54,6 +60,7 @@ const yamlify_json = (options) => {
 /**
  * Takes a path to a JSON file which will be transformed into TOML
  * @param { Object } options - Program options
+ * @param { String } options.path - Path to JSON file to be morphed into TOML
  **/
 const tomlify_json = (options) => {
     const json_string = fs.readFileSync(options.path, { "encoding": "utf8" });
@@ -68,6 +75,7 @@ const tomlify_json = (options) => {
 /**
  * Takes a path to a markdown file which will be transformed into HTML
  * @param { Object } options - Program options
+ * @param { String } options.path - Path to Markdown file to be morphed HTML
  **/
 const htmlify_markdown = (options) => {
     const markdown_string = fs.readFileSync(options.path, { "encoding": "utf8" });
@@ -79,6 +87,7 @@ const htmlify_markdown = (options) => {
 /**
  * Takes a path to an HTML file which will be transformed into Markdown
  * @param { Object } options - Program options
+ * @param { String } options.path - Path to HTML file to be morphed into Markdown
  **/
 const markdownify_html = (options) => {
     /**
@@ -91,4 +100,49 @@ const markdownify_html = (options) => {
     console.log(converter.makeMarkdown(html_string));
 }
 
-export { xmlify_json, markdownify_html, htmlify_markdown, jsonify_xml, yamlify_json, tomlify_json };
+/**
+ * Takes a path to a JSON file which will be transformed into CSV
+ * @param { Object } options - Program options
+ * @param { String } options.path - Path to JSON file to be morphed into CSV
+ **/
+const csvify_json = (options) => {
+    const json = JSON.parse(fs.readFileSync(options.path, { "encoding": "utf8" }));
+    const csv_string = Papaparse.unparse([json.Flow]);
+    console.log(csv_string);
+}
+
+/**
+ * Takes a path to an CSV file which will be transformed into JSON
+ * @param { Object } options - Program options
+ * @param { String } options.path - Path to CSV file to be morphed into JSON
+ **/
+const jsonify_csv = (options) => {
+    const csv_string = fs.readFileSync(options.path, { "encoding": "utf8" });
+    const csv_json = Papaparse.parse(csv_string);
+    console.log(JSON.stringify(csv_json.data));
+}
+
+/**
+ * Takes a path to an HTML file which will be transformed into Plain Text
+ * @param { Object } options - Program options
+ * @param { String } options.path - Path to HTML file to be morphed into Plain Text
+ **/
+const textify_html = (options) => {
+    const html_string = fs.readFileSync(options.path, { "encoding": "utf8" });
+    const html_plain_text = htmlToText(html_string, {
+        "wordwrap": 130
+    });
+    console.log(html_plain_text);
+}
+
+export {
+    csvify_json,
+    htmlify_markdown,
+    jsonify_csv,
+    jsonify_xml,
+    markdownify_html,
+    textify_html,
+    tomlify_json,
+    xmlify_json,
+    yamlify_json
+};
